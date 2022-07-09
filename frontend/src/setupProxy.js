571de9705a0ad21ab646f10e9d83a3268d914faa
YejiGong <87148580+YejiGong@users.noreply.github.com>
2022-07-09 22:36:08 +0900

    Update setupProxy.js

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 module.exports = function(app){
     app.use(
         createProxyMiddleware('/api',{
-            target: 'http://localhost:5000',
+            target: 'http://15.164.166.89',
             changeOrigin:true,
         })
     )
