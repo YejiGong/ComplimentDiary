34235e3247603f0474a6dc51770a27a470f54837
YejiGong <87148580+YejiGong@users.noreply.github.com>
2022-07-09 23:08:56 +0900

    Update setupProxy.js

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 module.exports = function(app){
     app.use(
         createProxyMiddleware('/api',{
-            target: 'http://15.164.166.89',
+            target: 'https://diary-api.haenu.xyz',
             changeOrigin:true,
         })
     )
